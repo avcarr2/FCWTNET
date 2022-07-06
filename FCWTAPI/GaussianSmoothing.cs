d210a0d72285a2bc8271715d76c2e8b9386d6210
zdanaceau <danaceau@wisc.edu>
2022-07-06 16:45:03 -0500

    Implemented 2D Gaussian Smoothing

---
+++ FCWTAPI/GaussianSmoothing.cs
@@ -0,0 +1,89 @@
+﻿
+namespace FCWT.NET
+{
+    public class GaussianSmoothing
+    {
+        // Code Below is meant to calculate the gaussian kernal in 1D
+        // This first method calculated the kernal based on a single stdev and the desired size of the kernal
+        public static double[,] Calculate1DSampleKernel(double deviation, int size)
+        {
+            double[,] ret = new double[size, 1];
+            double sum = 0;
+            int half = size / 2;
+            for (int i = 0; i < size; i++)
+            {
+                ret[i, 0] = 1 / (Math.Sqrt(2 * Math.PI) * deviation) * Math.Exp(-(i - half) * (i - half) / (2 * deviation * deviation));
+                sum += ret[i, 0];
+            }
+            return ret;
+        }
+        // Calculates 1D kernal based on stdev alone setting radius to 3*stdev + 1
+        public static double[,] Calculate1DSampleKernel(double deviation)
+        {
+            int size = (int)Math.Ceiling(deviation * 3) * 2 + 1;
+            return Calculate1DSampleKernel(deviation, size);
+        }
+        // Calculates a normalized 1d sample kernal 
+        public static double[,] CalculateNormalized1DSampleKernel(double deviation)
+        {
+            return NormalizeMatrix(Calculate1DSampleKernel(deviation));
+        }
+        public static double[,] NormalizeMatrix(double[,] matrix)
+        {
+            double[,] ret = new double[matrix.GetLength(0), matrix.GetLength(1)];
+            double sum = 0;
+            for (int i = 0; i < ret.GetLength(0); i++)
+            {
+                for (int j = 0; j < ret.GetLength(1); j++)
+                    sum += matrix[i, j];
+            }
+            if (sum != 0)
+            {
+                for (int i = 0; i < ret.GetLength(0); i++)
+                {
+                    for (int j = 0; j < ret.GetLength(1); j++)
+                        ret[i, j] = matrix[i, j] / sum;
+                }
+            }
+            return ret;
+        }
+        // Calculates a gaussian convolution from a given double matrix splitting the calc into
+        // x and y direction convolution are calculated separately
+        public static double[,] GaussianConvolution(double[,] matrix, double deviation)
+        {
+            double[,] kernel = CalculateNormalized1DSampleKernel(deviation);
+            double[,] res1 = new double[matrix.GetLength(0), matrix.GetLength(1)];
+            double[,] res2 = new double[matrix.GetLength(0), matrix.GetLength(1)];
+            //x-direction
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                // Calculates each point in the x direction
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    res1[i, j] = ProcessPoint(matrix, i, j, kernel, 0);
+            }
+            //y-direction
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                //Calculates each point in the y direction
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                    res2[i, j] = ProcessPoint(res1, i, j, kernel, 1);
+            }
+            return res2;
+        }
+        private static double ProcessPoint(double[,] matrix, int x, int y, double[,] kernel, int direction)
+        {
+            double res = 0;
+            int half = kernel.GetLength(0) / 2;
+            for (int i = 0; i < kernel.GetLength(0); i++)
+            {
+                int cox = direction == 0 ? x + i - half : x; // Set cox
+                int coy = direction == 1 ? y + i - half : y;
+                if (cox >= 0 && cox < matrix.GetLength(0) && coy >= 0 && coy < matrix.GetLength(1))
+                {
+                    res += matrix[cox, coy] * kernel[i, 0];
+                }
+            }
+            return res;
+        }
+    }
+}
--- TestFCWTAPI/UnitTest1.cs
+++ TestFCWTAPI/UnitTest1.cs
@@ -11,20 +11,20 @@ namespace TestFCWTAPI
         [SetUp]
         public void Setup()
         {
-            
+
         }
         [Test]
         public void TestFCWT()
         {
             double[] testValues = new double[1000];
-            double constant = 1D / 1000D * 2D * Math.PI; 
+            double constant = 1D / 1000D * 2D * Math.PI;
             for (int i = 0; i < 1000; i++)
             {
-                double val = (double)i * constant; 
+                double val = (double)i * constant;
                 testValues[i] = val;
             }
             double[] cosine = FunctionGenerator.TransformValues(testValues, FunctionGenerator.GenerateCosineWave);
-            float[][] results = FCWTAPI.CWT(cosine, 1, 6, 200, (float)(2 * Math.PI), 4, false); 
+            float[][] results = FCWTAPI.CWT(cosine, 1, 6, 200, (float)(2 * Math.PI), 4, false);
             Assert.AreEqual(200 * 6 * 2, results.GetLength(0));
         }
         [Test]
@@ -32,7 +32,7 @@ namespace TestFCWTAPI
         {
             double[] testArray = new double[] { 0D, 1D, 2D };
             float[] fArray = FCWTAPI.ConvertDoubleToFloat(testArray);
-            Assert.AreEqual((float)testArray[1], fArray[1]); 
+            Assert.AreEqual((float)testArray[1], fArray[1]);
         }
         [Test]
         public void TestFixOutputArray()
@@ -40,11 +40,11 @@ namespace TestFCWTAPI
             float[] testArray = new float[]
             {1F, 2F, 3F,
             4F, 5F, 6F,
-            7F, 8F, 9F, 
+            7F, 8F, 9F,
             10F, 11F, 12F};
 
             float[][] outputArray = FCWTAPI.FixOutputArray(testArray, 3, 1, 2);
-            Assert.AreEqual(4, outputArray.GetLength(0)); 
+            Assert.AreEqual(4, outputArray.GetLength(0));
         }
 
 
@@ -81,7 +81,7 @@ namespace TestFCWTAPI
                 new float[] {11, 12, 13, 14, 15, 16, 22}
             };
             Assert.Throws<IndexOutOfRangeException>(() => FCWTAPI.ToTwoDArray(badJaggedArray3));
-
+        }
         [Test]
         public void TestSplitIntoRealAndImaginary()
         {
@@ -94,7 +94,7 @@ namespace TestFCWTAPI
             FCWTAPI.SplitIntoRealAndImaginary(testArray, out float[][] realArray,
                 out float[][] imaginaryArray);
             Assert.AreEqual(4, realArray[0].Length);
-            Assert.AreEqual(4, imaginaryArray[0].Length); 
+            Assert.AreEqual(4, imaginaryArray[0].Length);
         }
         [Test]
         public void TestCalculatePhase()
@@ -107,7 +107,7 @@ namespace TestFCWTAPI
 
             float[][] phaseArray = FCWTAPI.CalculatePhase(testArray, testArray);
 
-            Assert.AreEqual(1.273, phaseArray[0][0], 0.001); 
+            Assert.AreEqual(1.273, phaseArray[0][0], 0.001);
         }
         [Test]
         public void TestCalculateModulus()
@@ -120,11 +120,11 @@ namespace TestFCWTAPI
 
             float[][] modArray = FCWTAPI.CalculateModulus(testArray, testArray);
             Console.WriteLine(string.Join("; ", modArray[0].AsEnumerable()));
-            Assert.AreEqual(1.41421, modArray[0][0], 0.001); 
+            Assert.AreEqual(1.41421, modArray[0][0], 0.001);
 
         }
         [Test]
-        public void testPreformCWT()
+        public void TestPreformCWT()
         {
             double[] testValues = new double[1000];
             double constant = 1D / 1000D * 2D * Math.PI;
@@ -139,6 +139,76 @@ namespace TestFCWTAPI
             Assert.AreEqual(cosineCWT.OutputCWT.GetLength(0), 200 * 6 * 2);
             Assert.AreEqual(cosineCWT.OutputCWT.GetLength(1), 1000);
         }
-
+        [Test]
+        public void TestCalculateSampleKernal()
+        {
+            double gaussDeviation = 1;
+            int gaussSize = 7;
+            int checkOne = 1;
+            double checkPoint1 = 1 / (Math.Sqrt(2 * Math.PI) * 1) * Math.Exp(-(checkOne - gaussSize/2) * (checkOne - gaussSize/2) / (2 * gaussDeviation * gaussDeviation));
+            int checkTwo = 5;
+            double checkPoint2 = 1 / (Math.Sqrt(2 * Math.PI) * 1) * Math.Exp(-(checkTwo - gaussSize / 2) * (checkTwo - gaussSize / 2) / (2 * gaussDeviation * gaussDeviation));
+            double[,] deviationKernal = GaussianSmoothing.Calculate1DSampleKernel(gaussDeviation);
+            double[,] allparamsKernal = GaussianSmoothing.Calculate1DSampleKernel(gaussDeviation, gaussSize);
+            Assert.AreEqual(deviationKernal, allparamsKernal);
+            Assert.AreEqual(checkPoint1, deviationKernal[1, 0]);
+            Assert.AreEqual(checkPoint2, deviationKernal[5, 0]);
+        }
+        [Test]
+        public void TestNormalizeMatrix()
+        {
+            double[,] unnormalizedMatrix = new double[,] { { 5, 7, 9 }, { 3, 4, 7 }, { 5, 6, 7 } };
+            double unnormalizedRatio1 = unnormalizedMatrix[0, 2] / unnormalizedMatrix[1, 3];
+            double unnormalizedRatio2 = unnormalizedMatrix[1, 2] / unnormalizedMatrix[2, 3];
+            double[,] normalizedMatrix = GaussianSmoothing.NormalizeMatrix(unnormalizedMatrix);
+            double normalizedRatio1 = normalizedMatrix[0, 2] / normalizedMatrix[1, 3];
+            double normalizedRatio2 = normalizedMatrix[1, 2] / normalizedMatrix[2, 3];
+            Assert.AreEqual(normalizedRatio1, unnormalizedRatio1);
+            Assert.AreEqual(normalizedRatio2, unnormalizedRatio2);
+            double normalizedSum = 0;
+            for(int i = 0; i < unnormalizedMatrix.GetLength(0); i++)
+            {
+                for(int j = 0; j < unnormalizedMatrix.GetLength(1); j++)
+                {
+                    normalizedSum = normalizedSum + normalizedMatrix[i, j];
+                }
+            }
+            Assert.AreEqual(normalizedSum, unnormalizedRatio1, 0.001);
+            
+        }
+        [Test]
+        public void TestProcessPoint()
+        {
+            double[,] test2DArray = new double[51, 51];
+            var pointArray = new (int, int, double)[]
+            {
+                (8, 8, 1),
+                (27, 8, 1),
+                (43, 2, 1),
+                (50, 50, 1)
+            };
+            for(int i = 0; i < 51; i++)
+            {
+                for (int j = 0; j < 50; j++)
+                {
+                    for (int t = 0; t < 50; t++)
+                        if (i == pointArray[t].Item1 && j == pointArray[t].Item2)
+                        {
+                            test2DArray[i, j] = pointArray[t].Item3;
+                        }
+                        else { test2DArray[i, j] = 0; }
+                }
+                
+            }
+            double[] valueArray = new double[4];
+            double[,] test1dKernal = GaussianSmoothing.CalculateNormalized1DSampleKernel(1);
+            for (int p = 0; p < 4; p++)
+            {
+                valueArray[p] = GaussianSmoothing.ProcessPoint(test2DArray, pointArray[p].Item1, pointArray[p].Item2, test1dKernal, 0);
+            }
+        }
     }
+
+
+    
 }
