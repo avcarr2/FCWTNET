7ac6d291800ed335e2dc1910393146347d5338c9
zdanaceau <danaceau@wisc.edu>
2022-07-26 16:29:24 -0500

    Added method to get frequency axis indices from preset frequencies.

--- FCWTAPI/CWTFrequencies.cs
+++ FCWTAPI/CWTFrequencies.cs
@@ -10,11 +10,12 @@ namespace FCWTNET
     {
         public double[]? WaveletCenterFrequencies { get; private set; }
         public int Length { get; private set; }
-        
-        public CWTFrequencies(double[] centerFrequencies)
+        public int Pnbvoice { get; private set; }
+        public CWTFrequencies(double[] centerFrequencies, int pnbvoice)
         {
             WaveletCenterFrequencies = centerFrequencies;
             Length = centerFrequencies.Length;
+            Pnbvoice = pnbvoice;
         }
         public CWTFrequencies()
         {
@@ -37,6 +38,59 @@ namespace FCWTNET
                 mzValues[i] = calibrationCoefficient / (WaveletCenterFrequencies[i] * WaveletCenterFrequencies[i]); 
             }
             return mzValues; 
+        }
+        public (int, int) CalculateIndicesForFrequencyRange(double startFrequency, double endFrequency)
+        {          
+
+            if (WaveletCenterFrequencies == null)
+            {
+                throw new ArgumentNullException(nameof(WaveletCenterFrequencies), "WaveletCenterFrequencies cannot be null");
+            }
+            if (WaveletCenterFrequencies[0] > startFrequency)
+            {
+                throw new ArgumentException(nameof(startFrequency), "startFrequency cannot be less than the minimum frequency");
+            }
+            if (startFrequency >= endFrequency)
+            {
+                throw new ArgumentException(nameof(endFrequency), "endFrequency must be greater than startFrequency");
+            }
+            if (WaveletCenterFrequencies[^1] < endFrequency)
+            {
+                throw new ArgumentException(nameof(endFrequency), "endFrequency must not be greater than the maximum CWT frequency");
+            }
+            if (WaveletCenterFrequencies[^2] <= startFrequency)
+            {
+                return (WaveletCenterFrequencies.Length - 2, WaveletCenterFrequencies.Length - 1);
+            }
+            else
+            {
+                int rawStartIndex = Array.BinarySearch(WaveletCenterFrequencies, startFrequency);
+                double axisStartFrequency;
+                int axisStartIndex;
+                int positiveStartIndex;
+                if (rawStartIndex < 0)
+                {
+                    positiveStartIndex = rawStartIndex * -1 - 1;
+                }
+                else
+                {
+                    positiveStartIndex = rawStartIndex;
+                }
+
+                if (WaveletCenterFrequencies[positiveStartIndex] > startFrequency)
+                {
+                    axisStartIndex = positiveStartIndex - 1;
+                }
+                else
+                {
+                    axisStartIndex = positiveStartIndex;
+                }
+                axisStartFrequency = WaveletCenterFrequencies[positiveStartIndex];
+                double deltaA = 1 / Convert.ToDouble(Pnbvoice);
+                int numFreqs = Convert.ToInt32(Math.Ceiling(Math.Log2(endFrequency / axisStartFrequency) / deltaA));
+                int axisEndIndex = axisStartIndex + numFreqs;
+                return (axisStartIndex, axisEndIndex);
+            }
         }
         public void CalculateFrequencyFromScale()
         {
