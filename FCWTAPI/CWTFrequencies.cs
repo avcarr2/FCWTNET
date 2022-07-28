8e83af01a137db68d2b238d7ac3b06a5a0bcae29
Zach Danaceau <100874533+zdanaceau@users.noreply.github.com>
2022-07-28 08:34:37 -0500

    CWT frequencies, convert frequency range to axis indices (#26)
    * Added method to get frequency axis indices from preset frequencies.
    
    * Migrated the calculation of the frequency axis indices to the CWTFrequencies.
    
    Co-authored-by: zdanaceau <danaceau@wisc.edu>
    Co-authored-by: Austin Carr <64652734+avcarr2@users.noreply.github.com>

--- FCWTAPI/CWTFrequencies.cs
+++ FCWTAPI/CWTFrequencies.cs
@@ -10,11 +10,15 @@ namespace FCWTNET
     {
         public double[]? WaveletCenterFrequencies { get; private set; }
         public int Length { get; private set; }
-        
-        public CWTFrequencies(double[] centerFrequencies)
+        public int Pnbvoice { get; private set; }
+        // Knowing C0 will likely be necessary downstream for knowing the actual frequencies
+        public float C0 {  get; private set; }
+        public CWTFrequencies(double[] centerFrequencies, int pnbvoice, float c0)
         {
             WaveletCenterFrequencies = centerFrequencies;
             Length = centerFrequencies.Length;
+            Pnbvoice = pnbvoice;
+            C0 = c0;
         }
         public CWTFrequencies()
         {
@@ -37,6 +41,64 @@ namespace FCWTNET
                 mzValues[i] = calibrationCoefficient / (WaveletCenterFrequencies[i] * WaveletCenterFrequencies[i]); 
             }
             return mzValues; 
+        }
+        /// <summary>
+        /// Method to get starting and ending indices for a particular frequency range.
+        /// This is intentionally greedy in the sense that the index range will always include the desired start and end frequencies
+        /// </summary>
+        /// <param name="startFrequency">Desired starting frequency</param>
+        /// <param name="endFrequency">Desired ending frequency</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException"></exception>
+        /// <exception cref="ArgumentException"></exception>
+        public (int, int) CalculateIndicesForFrequencyRange(double startFrequency, double endFrequency)
+        {          
+
+            if (WaveletCenterFrequencies == null)
+            {
+                throw new NullReferenceException("WaveletCenterFrequencies is null");
+            }
+            if (WaveletCenterFrequencies[0] > startFrequency)
+            {
+                throw new ArgumentException("startFrequency cannot be less than the minimum frequency", nameof(startFrequency));
+            }
+            if (startFrequency >= endFrequency)
+            {
+                throw new ArgumentException("endFrequency must be greater than startFrequency", nameof(endFrequency));
+            }
+            if (WaveletCenterFrequencies[^1] < endFrequency)
+            {
+                throw new ArgumentException("endFrequency must not be greater than the maximum CWT frequency", nameof(endFrequency));
+            }
+            else
+            {
+                int rawStartIndex = Array.BinarySearch(WaveletCenterFrequencies, startFrequency);
+                double axisStartFrequency;
+                int axisStartIndex;
+                int positiveStartIndex;
+                if (rawStartIndex < 0)
+                {
+                    positiveStartIndex = rawStartIndex * -1 - 1;
+                }
+                else
+                {
+                    positiveStartIndex = rawStartIndex;
+                }
+
+                if (WaveletCenterFrequencies[positiveStartIndex] > startFrequency)
+                {
+                    axisStartIndex = positiveStartIndex - 1;
+                }
+                else
+                {
+                    axisStartIndex = positiveStartIndex;
+                }
+                axisStartFrequency = WaveletCenterFrequencies[positiveStartIndex];
+                double deltaA = 1 / Convert.ToDouble(Pnbvoice);
+                int numFreqs = 1 + Convert.ToInt32(Math.Ceiling(Math.Log2(endFrequency / axisStartFrequency) / deltaA));
+                int axisEndIndex = axisStartIndex + numFreqs;
+                return (axisStartIndex, axisEndIndex);
+            }
         }
         public void CalculateFrequencyFromScale()
         {
