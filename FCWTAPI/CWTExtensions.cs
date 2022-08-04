1728355accf1bb14dd3e92cc2bdb46a4fc9969b8
Zach Danaceau <100874533+zdanaceau@users.noreply.github.com>
2022-08-04 13:46:38 -0500

    Time axis compression (#35)
    * Added functionality to compress CWT features along the time axis.
    
    * Corrected comments on new methods.
    
    Co-authored-by: Austin Carr <64652734+avcarr2@users.noreply.github.com>

--- FCWTAPI/CWTExtensions.cs
+++ FCWTAPI/CWTExtensions.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FCWTNET
 {
-    public static  class CWTExtensions
+    public static class CWTExtensions
     {
         public static double GetFrequencyAtIndex(this CWTObject cwt, int index)
         {
@@ -16,8 +16,80 @@ namespace FCWTNET
             }
             else
             {
-                throw new NullReferenceException("cwt frequency axis is null"); 
+                throw new NullReferenceException("cwt frequency axis is null");
             }
+        }
+        /// <summary>
+        /// Method for compressing CWT features along the time axis.
+        /// Compresses the original double[,] to be the largest array with less columns than maxArrayColumns
+        /// with the same number of rows as the original array.
+        /// </summary>
+        /// <param name="data">Original double[,]</param>
+        /// <param name="compressedData">Output double[,]</param>
+        /// <param name="maxArrayColumns">Maximum number of columns in the output array</param>
+        public static void Time2DArrayCompression(double[,] data, out double[,] compressedData, int maxArrayColumns)
+        {
+            if (maxArrayColumns <= 0)
+            {
+                throw new ArgumentException("maxArrayColumns must be a positive integer", nameof(maxArrayColumns));
+            }
+            int compressionRatio = (int)Math.Ceiling(data.GetLength(1) / (double)maxArrayColumns);
+            int subDvisionCount = (int)Math.Ceiling((double)data.GetLength(1) / compressionRatio);
+            compressedData = new double[data.GetLength(0), subDvisionCount];
+            for (int i = 0; i < data.GetLength(0); i++)
+            {
+                for (int j = 0; j < subDvisionCount - 1; j++)
+                {
+                    double pointSum = 0;
+                    for (int k = 0; k < compressionRatio; k++)
+                    {
+                        pointSum += data[i, compressionRatio * j + k];
+                    }
+                    compressedData[i, j] = pointSum / compressionRatio;
+                }
+                double finalPointSum = 0;
+                double finalPointCount = 0;
+                for (int j = (subDvisionCount - 1) * compressionRatio; j < data.GetLength(1); j++)
+                {
+                    finalPointSum += data[i, j];
+                    finalPointCount += 1;
+                }
+                compressedData[i, compressedData.GetLength(1) - 1] = finalPointSum / finalPointCount;
+            }
+        }
+        /// <summary>
+        /// Method for compressing CWT time axis.
+        /// Compresses the original double[] to be the largest array with less elements than maxArrayColumns
+        /// </summary>
+        /// <param name="data">Original double[]</param>
+        /// <param name="compressedData">Output double[]</param>
+        /// <param name="maxArrayColumns">Maximum number of elements in the output array</param>
+        public static void TimeAxisCompression(double[] data, out double[] compressedData, int maxArrayColumns)
+        {
+            if (maxArrayColumns <= 0)
+            {
+                throw new ArgumentException("maxArrayColumns must be a positive integer", nameof(maxArrayColumns));
+            }
+            int compressionRatio = (int)Math.Ceiling(data.Length / (double)maxArrayColumns);
+            int subDvisionCount = (int)Math.Ceiling((double)data.Length / compressionRatio);
+            compressedData = new double[subDvisionCount];
+            for (int j = 0; j < subDvisionCount - 1; j++)
+            {
+                double pointSum = 0;
+                for (int k = 0; k < compressionRatio; k++)
+                {
+                    pointSum += data[compressionRatio * j + k];
+                }
+                compressedData[j] = pointSum / compressionRatio;
+            }
+            double finalPointSum = 0;
+            double finalPointCount = 0;
+            for (int j = (subDvisionCount - 1) * compressionRatio; j < data.Length; j++)
+            {
+                finalPointSum += data[j];
+                finalPointCount += 1;
+            }
+            compressedData[compressedData.Length - 1] = finalPointSum / finalPointCount;
         }
     }
 }
